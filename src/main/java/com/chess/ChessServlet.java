eaa4f8309204e69b3b88d6250004513ef63fc318
otmojo <205744339+otmojo@users.noreply.github.com>
2026-02-12 15:37:31 +0900

    Update ChessServlet.java

--- src/main/java/com/chess/ChessServlet.java
+++ src/main/java/com/chess/ChessServlet.java
@@ -8,14 +8,14 @@ import com.chess.RoomManager;
 @WebServlet("/chessAction")
 public class ChessServlet extends HttpServlet {
 
-    // GET 用于轮询：获取对手的棋步
+    // GET
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String roomId = request.getParameter("roomId");
         String move = RoomManager.getMove(roomId);
         response.getWriter().write(move);
     }
 
-    // POST 用于提交：发送我的棋步
+    // POST
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String roomId = request.getParameter("roomId");
         String move = request.getParameter("move");

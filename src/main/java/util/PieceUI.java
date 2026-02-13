fe6dfe0466c02c39716eabc5f1ee7bc15a61e843
otmojo <205744339+otmojo@users.noreply.github.com>
2026-02-13 14:04:30 +0900

    Replace Unicode escape sequences with emojis

--- src/main/java/util/PieceUI.java
+++ src/main/java/util/PieceUI.java
@@ -3,21 +3,23 @@ package util;
 public class PieceUI {
     public static String getUnicode(String p) {
         if (p == null || p.trim().isEmpty()) return "";
+        
+        // use Emoji 
         switch(p) {
             // white
-            case "K": return "&#9812;"; // ♔
-            case "Q": return "&#9813;"; // ♕
-            case "R": return "&#9814;"; // ♖
-            case "B": return "&#9815;"; // ♗
-            case "N": return "&#9816;"; // ♘
-            case "P": return "&#9817;"; // ♙
+            case "K": return "♔";
+            case "Q": return "♕";
+            case "R": return "♖";
+            case "B": return "♗";
+            case "N": return "♘";
+            case "P": return "♙";
             // black
-            case "k": return "&#9818;"; // ♚
-            case "q": return "&#9819;"; // ♛
-            case "r": return "&#9820;"; // ♜
-            case "b": return "&#9821;"; // ♝
-            case "n": return "&#9822;"; // ♞
-            case "p": return "&#9823;"; // ♟
+            case "k": return "♚";
+            case "q": return "♛";
+            case "r": return "♜";
+            case "b": return "♝";
+            case "n": return "♞";
+            case "p": return "♟";
             default: return "";
         }
     }
